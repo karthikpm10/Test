8ddd9ac6a6ccec2c3c74a2d9db06b72ecdb18ee4
karthikpm10 <karthikpm10@gmail.com>
2017-07-12 00:27:00 -0400

    Added assignement1 android

---
+++ Assignement1/app/src/main/java/com/example/karthik/assignement1/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.karthik.assignement1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
